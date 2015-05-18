0385df148b13fe139dfb20e206bbe394910c84ff
Siavash Shibani <siawash.shibania@gmail.com>
2015-05-18 20:08:44 +0200

    adding missing files

---
+++ src/DD4T/DD4T.ContentModel.Contracts/Configuration/IDD4TConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DD4T.ContentModel.Contracts.Configuration
+{
+    public interface IDD4TConfiguration
+    {
+        int PublicationId { get; set; }
+    }
+}
